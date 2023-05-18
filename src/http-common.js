b8b4d25bce5876301fcce7159566439abdffff2b
Anthony Romero <44810112+antho1810@users.noreply.github.com>
2023-05-18 13:51:52 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -8,6 +8,6 @@ export default axios.create({
     baseURL: "https://sgacfi-back-mern.up.railway.app/sgacfi-api",
     headers: {
         "Content-Type": "application/json",
-        "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjQyMjkwMjAxMWY1NGI2YjliN2IxYSIsImlhdCI6MTY4NDI4NDA0OCwiZXhwIjoxNjg0ODg4ODQ4fQ.4DVSvi4sVHneQOlXJ-ledypR1K4mWPei0af8aODxmh8"
+        "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjY2ZjRiMGM5NDY2YzNmZTdmODdhYyIsImlhdCI6MTY4NDQzNDc2NCwiZXhwIjoxNjg1MDM5NTY0fQ.xSTLtRQ4PB6eOLbn9vHgHbh1iwgJVL_BtngPu8auEgU"
     }
 })
