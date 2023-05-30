cff44350fda168c4096b1719b70e74afcb2e434f
Anthony Romero <44810112+antho1810@users.noreply.github.com>
2023-05-30 14:17:37 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -5,7 +5,7 @@ const token = localStorage.getItem('token')
 console.log(token)
 
 export default axios.create({
-    baseURL: "https://sgacfi-back-mern.up.railway.app/sgacfi-api",
+    baseURL: "https://sgacfi-back-mern.up.railway.app/sgacfi-api/",
     headers: {
         "Content-Type": "application/json",
         "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDc2MzkzZDk4MDE5NzAyNTk0ZjkxYTciLCJub21icmUiOiJFZG5hIiwiYXBlbGxpZG8iOiJDb25kZSIsImNhcmdvIjoiU2VjcmV0YXJpYSIsInJvbCI6W10sImlhdCI6MTY4NTQ2OTUwMSwiZXhwIjoxNjg2MDc0MzAxfQ._y-H8G9sc6ZVu9NS_tuUCW9ChtDZridhdRepe_rIMAk"
